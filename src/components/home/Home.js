d4979c58707711bc940f967d3a047b9973b472a4
ryanlb777 <ryan.lebon24@gmail.com>
2019-02-17 13:42:10 -0800

    changes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,21 +1,26 @@
 import React, { Component } from "react";
 import "./Home.css";
+import "./test.scss";
+import background from '../../images/background.jpg'
+import profile from '../../images/profile_picture.png'
 
 class Home extends Component {
   render() {
     return (
       <div className="home-container">
+    
+
         <header className="home-header">
           <div className="grid-container">
             <div className="grid-item">
-              <h5> Ryan LeBon </h5>
-              <h5> Software Engineer </h5>
+              <h5> Name </h5>
+              <h6> Title </h6>
             </div>
             <div className="grid-item" />
             <div className="grid-item">
               <img
                 className="header-image"
-                src="https://via.placeholder.com/300"
+                src={profile}
               />
             </div>
           </div>
@@ -25,9 +30,9 @@ class Home extends Component {
         <img
           className="body-image"
           alt="img"
-          src="https://cdn.pixabay.com/photo/2015/03/26/09/48/chicago-690364_960_720.jpg"
+          src={background}
         />
-
+     
         <div className="grid-container-two">
           <div className="grid-item">1</div>
           <div className="grid-item">2</div>
